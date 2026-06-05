e9a862b85d863442bbc8fd2096b168833e0f0a79
Leonid Kuznetsov <91047299+lakuzn@users.noreply.github.com>
2026-06-05 18:24:02 +0300

    Delete blueprints directory

--- blueprints/tickets/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-tickets_bp = Blueprint(
-    "tickets",
-    __name__,
-    url_prefix="/tickets",
-    template_folder="../templates/tickets",
-    static_folder="../static",
-)
