fd462e70db9d1e7d6bf5ce5abfa84fb08cb2d4a6
Leonid Kuznetsov <91047299+lakuzn@users.noreply.github.com>
2026-06-05 18:24:51 +0300

    Delete utils directory

--- utils/forms.py
+++
@@ -1,9 +0,0 @@
-from flask import flash
-
-
-def flash_form_errors(form):
-    """Обработчик ошибок для WTForms"""
-    for field, errors in form.errors.items():
-        for error in errors:
-            label = getattr(form, field).label.text
-            flash(f"Ошибка в поле '{label}': {error}" "error")
