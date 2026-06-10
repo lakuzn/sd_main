daf816c7546fc82b72cad53ff168519aa57f460e
Leonid Kuznetsov <91047299+lakuzn@users.noreply.github.com>
2026-06-10 19:30:45 +0300

    Delete static directory

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-import { initAllComponents } from "./index.js";
-
-export { initAllComponents };
-
-document.addEventListener('DOMContentLoaded', async function () {
-    await initAllComponents();
-})
